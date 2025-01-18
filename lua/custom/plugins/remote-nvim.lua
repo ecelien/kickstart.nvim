11e29a2e6d129e1185f3861c01fb5e4f51ee450b
Brian Lien <briantlien@gmail.com>
2025-01-18 16:47:40 -0500

    feat: add remote-nvim

---
+++ lua/custom/plugins/remote-nvim.lua
@@ -0,0 +1,16 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+return {
+  {
+    'amitds1997/remote-nvim.nvim',
+    version = '*', -- Pin to GitHub releases
+    dependencies = {
+      'nvim-lua/plenary.nvim', -- For standard functions
+      'MunifTanjim/nui.nvim', -- To build the plugin UI
+      'nvim-telescope/telescope.nvim', -- For picking b/w different remote methods
+    },
+    config = true,
+  },
+}
